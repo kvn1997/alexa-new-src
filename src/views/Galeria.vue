9b8957ba04a8e0745de613357f43197d2e1702be
kvn1997 <57558336+kvn1997@users.noreply.github.com>
2020-01-22 19:18:04 +0100

    Algo de la galeria y un ejemplo de footer

--- src/views/Galeria.vue
+++ src/views/Galeria.vue
@@ -4,44 +4,48 @@
               <b-row class="justify-content-md-center">
 
 
-  <div class="card" style="width:400px">
+  <div  class="card, mt-5" style="width:400px">
   <img class="card-img-top" src="@/assets/imgs/15.jpg" alt="Card image">
-  <div class="card-body">
-    <h4 class="card-title">Family Sessions</h4>
+  <div  border-variant="primary"
+        
+        align="center" class="card-body">
+    <h4 class="card-title">Session De Familia</h4>
     <!-- <p class="card-text">P</p> -->
     <a href="#" class="btn btn-primary">Haz tu reserva!</a>
   </div>
 </div>
 
-  <b-col cols="12" md="auto" class="card" style="width:400px">
+  <div class="card mt-5" style="width:320px">
   <img class="card-img-top" src="@/assets/imgs/18.jpg" alt="Card image">
-  <div class="card-body">
+  <div align="center" class="card-body">
+    <h4 class="card-title">Pre-Mama</h4>
+    <a href="#" class="btn btn-primary">Haz tu reserva!</a>
+  </div>
+</div>
+
+  <div class="card, mt-5" style="width:340px">
+  <img class="card-img-top" src="@/assets/imgs/14.jpg" alt="Card image">
+  <div align="center" class="card-body">
     <h4 class="card-title">Parejas</h4>
     <a href="#" class="btn btn-primary">Haz tu reserva!</a>
   </div>
-</b-col>
-
-<div class="card" style="width:340px">  <img class="card-img-top" src="@/assets/imgs/14.jpg" alt="Card image">
-  <div class="card-body">
+</div>
+   <div class="card, m-5" style="width:400px">
+  <img class="card-img-top" src="@/assets/imgs/12.jpg" alt="Card image">
+  <div align="center" class="card-body">
     <h4 class="card-title">Comuniones</h4>
     <a href="#" class="btn btn-primary">Haz tu reserva!</a>
   </div>
 </div>
-   <b-col cols="12" md="auto" class="card" style="width:400px">
-  <img class="card-img-top" src="@/assets/imgs/12.jpg" alt="Card image">
-  <div class="card-body">
-    <h4 class="card-title">Pre-Mama</h4>
-    <a href="#" class="btn btn-primary">Haz tu reserva!</a>
-  </div>
-</b-col>
- <div class="card" style="width:400px">
+ <div class="card, m-5" style="width:400px">
   <img class="card-img-top" src="@/assets/imgs/2.jpg" alt="Card image">
-  <div class="card-body">
+  <div align="center" class="card-body">
     <h4 class="card-title">Retratos</h4>
     <a href="#" class="btn btn-primary">Haz tu reserva!</a>
   </div>
 </div> 
  </b-row>
+ 
 
 </b-container>
 </template>
